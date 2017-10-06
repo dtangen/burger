7bdf26587356f1d8980cd26d410b631cf98f52f0
dtangen <dtangen1@gmail.com>
2017-10-05 20:07:55 -0500

    hopefully final commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheeseburger', FALSE, current_time());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon', FALSE, current_time());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Big Mac', FALSE, current_time());
